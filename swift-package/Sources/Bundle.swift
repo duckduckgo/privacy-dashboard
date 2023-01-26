fe3eb73718e20ba2c2e61d726fa93edddafbaacf
Michal Smaga <miasma13@gmail.com>
2023-01-26 12:26:42 +0100

    Update bundle path

--- swift-package/Sources/Bundle.swift
+++ swift-package/Sources/Bundle.swift
@@ -5,9 +5,9 @@ import PrivacyDashboard_resources
 public extension Bundle {
     static var privacyDashboardURL: URL? {
         #if os(iOS)
-            privacyDashboardResourcesBundle.url(forResource: "ios", withExtension: "html", subdirectory: "app/html")
+            privacyDashboardResourcesBundle.url(forResource: "ios", withExtension: "html", subdirectory: "html")
         #elseif os(macOS)
-            privacyDashboardResourcesBundle.url(forResource: "macos", withExtension: "html", subdirectory: "app/html")
+            privacyDashboardResourcesBundle.url(forResource: "macos", withExtension: "html", subdirectory: "html")
         #else
             nil
         #endif
