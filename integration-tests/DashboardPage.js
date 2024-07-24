441ba9dd12dbb08d400f82b954603094770f8065
Marcos Gurgel <mgurgel@gmail.com>
2024-07-24 21:01:08 +0100

    Updated test with new copy

--- integration-tests/DashboardPage.js
+++ integration-tests/DashboardPage.js
@@ -155,7 +155,7 @@ export class DashboardPage {
     async hasAllowedFirstPartyText() {
         const { page } = this
         await expect(page.locator('#key-insight')).toContainText(
-            "We didn't find any external companies trying to load tracking requests on this page."
+            "We only found non-tracking requests or requests associated with example.com on this page."
         )
     }
 
