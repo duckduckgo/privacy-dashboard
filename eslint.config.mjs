f862b5d5a13d64093edfde12c48744b9c90576f3
Maxim Tsoy <maks.tsoy@gmail.com>
2024-11-01 13:29:17 +0100

    Upgrade eslint and use the shared ddg config

---
+++ eslint.config.mjs
@@ -0,0 +1,40 @@
+import tseslint from 'typescript-eslint';
+import ddgConfig from "@duckduckgo/eslint-config";
+import globals from "globals";
+
+// @ts-check
+export default tseslint.config(
+    ...ddgConfig,
+    ...tseslint.configs.recommended,
+
+    {
+        ignores: [
+            "**/build/",
+            "**/swift-package/",
+            "shared/data/constants.js",
+            "schema/__generated__",
+            "schema/__fixtures__",
+        ],
+    },
+    {
+        languageOptions: {
+            globals: {
+                ...globals.browser,
+                ...globals.webextensions,
+                ...globals.node,
+                it: true,
+                Proxy: true,
+            },
+        },
+
+        rules: {
+            "@typescript-eslint/no-explicit-any": "off",
+            "@typescript-eslint/ban-ts-comment": "off",
+            "@typescript-eslint/triple-slash-reference": "off",
+
+            "@typescript-eslint/no-unused-vars": ["error", {
+                argsIgnorePattern: "^_",
+            }],
+        },
+    }
+);
