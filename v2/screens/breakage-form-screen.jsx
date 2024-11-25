7a3801fa8a367373840df7c1af0c9103123407b3
Marcos Gurgel <mgurgel@gmail.com>
2024-11-25 15:26:51 +0000

    Revert "Form size fix"
    This reverts commit 25d4e5372cff51d2d05db138ccb95c8cb34ffa95.

--- v2/screens/breakage-form-screen.jsx
+++ v2/screens/breakage-form-screen.jsx
@@ -302,8 +302,6 @@ function DetailsDisclosureMessage() {
 
     // Animates height of details container
     useLayoutEffect(() => {
-        if (platform.name !== 'android' && platform.name !== 'ios') return;
-
         let height = 0;
 
         if (detailsRef.current) {
@@ -317,13 +315,11 @@ function DetailsDisclosureMessage() {
             }
 
             detailsElement.style.height = `${height}px`;
-            detailsElement.dataset.open = `${detailsOpen}`;
         }
     });
 
     const toggleHandler = (e) => {
-        const detailsElement = /** @type HTMLDetailsElement */ (e.target);
-        setDetailsOpen(detailsElement.open);
+        setDetailsOpen(/** @type HTMLDetailsElement */ (e.target).open);
     };
 
     return (
