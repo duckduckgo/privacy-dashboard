2b89e2b195af7638f2f66730212c9e2f3f00594b
Marcos Gurgel <mgurgel@gmail.com>
2024-12-12 11:02:54 +0000

    Button refactor

--- shared/js/ui/components/button.jsx
+++ shared/js/ui/components/button.jsx
@@ -1,15 +1,17 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import { h } from 'preact';
+import cn from 'classnames';
 
 /**
  * @typedef {object} ComponentProps
  * @property {"standard" | "desktop-vibrancy" | "desktop-standard" | "ios-secondary" | "macos-standard"} [variant]
  * @property {"big" | "desktop-large" | "small"} [btnSize]
+ * @property {string} [className]
  * @param {import("preact").ComponentProps<'button'> & ComponentProps} props
  */
-export function Button({ children, btnSize, variant = 'desktop-vibrancy', ...rest }) {
+export function Button({ children, className='', btnSize, variant = 'desktop-vibrancy', ...rest }) {
     return (
-        <button type="button" className="button token-body" {...rest} data-variant={variant} data-size={btnSize}>
+        <button type="button" className={cn(["button token-body", className])} {...rest} data-variant={variant} data-size={btnSize}>
             {children}
         </button>
     );
