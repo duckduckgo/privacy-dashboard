19e2ffc57cfe7fab9371b17d2861e45c9f77babb
Marcos Gurgel <mgurgel@gmail.com>
2024-12-20 10:25:31 +0000

    Other category always last

--- integration-tests/macos.spec-int.js
+++ integration-tests/macos.spec-int.js
@@ -127,6 +127,18 @@ test.describe('breakage form', () => {
         await dash.mocks.calledForSubmitBreakageForm({ category: 'layout', description: '' });
     });
 
+    test('other category is at the end of list', async ({ page }) => {
+        /** @type {DashboardPage} */
+        const dash = await DashboardPage.webkit(page, {
+            screen: 'breakageForm',
+            randomisedCategories: 'true',
+            platform: 'macos',
+        });
+        await dash.addState([testDataStates.google]);
+        await dash.selectsCategoryType('The site is not working as expected', 'notWorking');
+        await dash.categoryIsLast('Something else');
+    });
+
     test('hides description prompt on "dislike" category', { tag: '@screenshots' }, async ({ page }) => {
         /** @type {DashboardPage} */
         const dash = await DashboardPage.webkit(page, {
