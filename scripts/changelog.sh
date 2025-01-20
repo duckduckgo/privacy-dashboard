f7e531dd95003dc7915e330101532b0bdbfef287
Shane Osbourne <shane.osbourne8@gmail.com>
2025-01-20 10:32:57 +0000

    updates

--- scripts/changelog.sh
+++ scripts/changelog.sh
@@ -1,1 +1,1 @@
-git log main --since "$(git show -s --format=%ci $(git rev-list --tags --max-count=1))" --pretty='format:- %s'
+git log "$SOURCE_BRANCH" --since "$(git show -s --format=%ci $(git rev-list --tags --max-count=1))" --pretty='format:- %s'
