aeabf431a7a4e6bdcc030829ffe17121a8b52a70
Shane Osbourne <shane.osbourne8@gmail.com>
2025-03-11 20:52:39 +0000

    nav: POC fixing reduced motion navigation

--- v2/navigation.jsx
+++ v2/navigation.jsx
@@ -164,6 +164,36 @@ function navReducer(state, event) {
                         via: 'pop',
                     };
                 }
+                case 'popstate': {
+                    console.groupCollapsed('popstate');
+                    const stack = state.stack.filter(x=>x!=='primaryScreen')
+                    const commit = state.commit.filter(x=>x!=='primaryScreen')
+                    const current = new URLSearchParams(location.search).getAll('stack').filter(x=>x!=='primaryScreen')
+                    console.log('  state.stack', stack);
+                    console.log('  state.commit', commit);
+                    console.log('  url current', current);
+                    console.groupEnd();
+                    if (current.length < stack.length) {
+                        if (!event.opts.animate) {
+                            const next = state.stack.slice(0, -1);
+                            return {
+                                ...state,
+                                commit: next,
+                                stack: next,
+                                state: /** @type {const} */ ('settled'),
+                                via: 'pop',
+                            };
+                        }
+                        return {
+                            ...state,
+                            commit: state.stack,
+                            stack: state.stack.slice(0, -1),
+                            state: /** @type {const} */ ('transitioning'),
+                            via: 'pop',
+                        };
+                    }
+                    return state;
+                }
                 default: {
                     console.warn('ignoring', event, 'state', state);
                     return state;
@@ -178,9 +208,10 @@ function navReducer(state, event) {
 /**
  * @typedef {{ animate: boolean }} ActionOpts
  * @typedef {{ type: 'push', name: ScreenName, opts: ActionOpts}
- *   | {type: 'pop', opts: ActionOpts}
+ *   | {type: 'pop', opts: ActionOpts }
  *   | {type: 'replace', stack: ScreenName[], opts: ActionOpts}
  *   | {type: 'end'}
+ *   | {type: 'popstate'; opts: ActionOpts }
  * } NavEvent
  * @typedef {{
  *    commit: string[],
@@ -231,18 +262,7 @@ export function Navigation(props) {
          * - otherwise, it's just a 'back' action, so we can `pop` an item from the stack as usual
          */
         function popstateHandler() {
-            const currentUrlParams = new URLSearchParams(location.search);
-            const currentURLStack = currentUrlParams.getAll('stack');
-            const navigationIntentionIsForwards = currentURLStack.length > state.stack.length;
-
-            if (navigationIntentionIsForwards) {
-                const lastEntry = currentURLStack[currentURLStack.length - 1];
-                if (isScreenName(lastEntry)) {
-                    dispatch({ type: 'push', name: lastEntry, opts: { animate: props.animate && isAndroid() } });
-                }
-            } else {
-                dispatch({ type: 'pop', opts: { animate: props.animate && isAndroid() } });
-            }
+            dispatch({ type: 'popstate', opts: { animate: props.animate } })
         }
 
         window.addEventListener('popstate', popstateHandler);
@@ -318,6 +338,7 @@ export function Navigation(props) {
             // Current screen will be popped right after
             const primaryScreen = 'primaryScreen';
             const newStack = /** @type {ScreenName[]} */ ([primaryScreen]);
+
             if (state.stack.length > 1) {
                 newStack.push(state.stack.slice(-1)[0]);
             }
